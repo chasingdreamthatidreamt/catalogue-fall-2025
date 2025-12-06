99cc5b7795f258074ca28ab4d05701e9b185e2da
chasingdreamthatidreamt <143831135+chasingdreamthatidreamt@users.noreply.github.com>
2025-12-06 00:12:22 -0700

    updating the docker file

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$title = "Catelogue";
+include 'includes/header.php';
+include 'includes/filters.php';
+include 'includes/footer.php';
+?>
+
+<main class="container">
+    <section class="row my-5">
+        <!-- Introduction -->
+        <div class="col-md-10 col-lg-8 col-xxl-6 mb-4">
+            <h2 class="display-4">Welcome to <span class="d-block text-danger">Food World</span></h2>
+        </div>
+    </section>
+
+</main>
