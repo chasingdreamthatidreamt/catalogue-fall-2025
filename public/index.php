52e3414cc32886e106be85f918d4b958dbbf7775
chasingdreamthatidreamt <143831135+chasingdreamthatidreamt@users.noreply.github.com>
2025-12-09 20:58:10 -0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,17 +1,26 @@
 <?php
-
-$title = "Catelogue";
+//$title = "Catalogue";
 include 'includes/header.php';
-include 'includes/filters.php';
-include 'includes/footer.php';
+include 'includes/filter.php';
 ?>
 
 <main class="container">
+
+    <!-- Intro Section -->
     <section class="row my-5">
-        <!-- Introduction -->
-        <div class="col-md-10 col-lg-8 col-xxl-6 mb-4">
-            <h2 class="display-4">Welcome to <span class="d-block text-danger">Food World</span></h2>
+        <div class="col-md-10 col-lg-8 col-xxl-6 mb-4 item-align-middle">
+            <h2 class="display-4 ">
+                Welcome to <span class="d-block text-danger">Food World</span>
+            </h2>
+            <p class="lead">
+                Explore our delicious catalogue of foods. Browse items, check details,
+                and find your next favourite dish!
+            </p>
         </div>
     </section>
 
+    
+
 </main>
+
+<?php include 'includes/footer.php'; ?>
